589ac264d6eb11472cde54e6f84eb043da9fea09
hacktheegg <jarrahvanritten@gmail.com>
2023-02-23 16:46:43 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Format your values like this: ");
+        Console.WriteLine("[folderPath] [searchedFileExtention]");
+        Console.Write(@"FileExtentionTracker\: ");
+        string input = Console.ReadLine();
+
+        string[] dirs = Directory.GetDirectories(input.Split(' ')[0], );
+    }
+}
