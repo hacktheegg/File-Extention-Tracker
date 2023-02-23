1ee2ffdb516e9a2c5e461b3b84bf43abb35a8b77
hacktheegg <jarrahvanritten@gmail.com>
2023-02-23 17:35:14 +1100

    1st part - done

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 ﻿
 
+
+
+
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.Write("Format your values like this: ");
-        Console.WriteLine("[folderPath] [searchedFileExtention]");
+        Console.WriteLine("[folderPath] * [searchedFileExtention]");
         Console.Write(@"FileExtentionTracker\: ");
         string input = Console.ReadLine();
 
-        string[] dirs = Directory.GetDirectories(input.Split(' ')[0], );
+        string[] dirs = Directory.GetFiles(input.Split(" * ")[0], "*" + input.Split(" * ")[1], SearchOption.AllDirectories);
+
+        for (int i = 0; i < dirs.Length; i++)
+        {
+            Console.WriteLine((i + 1) + ". " + dirs[i]);
+        }
+
+        Console.WriteLine("done");
+
+        Console.ReadLine();
     }
 }
